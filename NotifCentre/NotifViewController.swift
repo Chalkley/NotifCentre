5c56a8bbcbfe2c87e14ce51c3a90a12676998b9b
Jason Chalkley <jaychalks@gmail.com>
2019-01-31 16:35:40 +0000

    Notification added to both VC. Random array added to home VC.

--- NotifCentre/NotifViewController.swift
+++ NotifCentre/NotifViewController.swift
@@ -8,12 +8,45 @@
 
 import UIKit
 
+extension Notification.Name {
+    static let changeColor = Notification.Name(rawValue: "changeColor")
+}
+
 class NotifViewController: UIViewController {
 
+    @IBOutlet weak var changeButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupUI()
+        
       
+    }
+    @IBAction func changeTapped(_ sender: Any) {
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "changeColor"), object: nil)
+        dismiss(animated: true, completion: nil)
+        print("Change tapped, notif VC Dismissed.")
+    }
+    
+    func setupUI() {
+        let leftNavButton = UIButton(type: .custom)
+        leftNavButton.setTitle("Back", for: .normal)
+        leftNavButton.setTitleColor(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1), for: .normal)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: leftNavButton)
+        
+        leftNavButton.addTarget(self, action: #selector(NotifViewController.backTapped), for: .touchUpInside)
+        
+        changeButton.setTitle("Hit me", for: .normal)
+        changeButton.titleLabel?.font = UIFont(name: "AvenirNext-Medium", size: 18)
+        changeButton.backgroundColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+        changeButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        
+    }
+    
+    @objc func backTapped() {
+        dismiss(animated: true, completion: nil)
+
     }
     
 
--- NotifCentre/ViewController.swift
+++ NotifCentre/ViewController.swift
@@ -8,12 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPopoverPresentationControllerDelegate {
 
+    var randomColor : [UIColor]?
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        randomColor =  [#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1), #colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1), #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1), #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1), #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1), #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1), #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)]
+        NotificationCenter.default.addObserver(self, selector: #selector(self.changeBG), name: NSNotification.Name(rawValue: "changeColor"), object: nil)
+        
         setupUI()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @objc func changeBG() {
+        if let random = randomColor!.randomElement() {
+            view.backgroundColor = random
+            
+        }
     }
     
     func setupUI() {
@@ -22,8 +35,23 @@ class ViewController: UIViewController {
         rightNavUtton.setTitleColor(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1), for: .normal)
         navigationItem.rightBarButtonItem = UIBarButtonItem(customView: rightNavUtton)
         
+        rightNavUtton.addTarget(self, action: #selector(self.navTapped), for: .touchUpInside)
+        
     }
 
+    @objc func navTapped() {
+        let popoverContent = self.storyboard?.instantiateViewController(withIdentifier: "NotifViewController") as! NotifViewController
+        let nav = UINavigationController(rootViewController: popoverContent)
+        nav.modalPresentationStyle = UIModalPresentationStyle.popover
+        let popover = nav.popoverPresentationController
+        popoverContent.preferredContentSize = CGSize(width: 300, height: 300)
+        popover!.delegate = self
+        popover!.sourceView = self.view
+        popover?.sourceRect = CGRect(x: 100, y: 100, width: 0, height: 0)
+        
+        self.present(nav, animated: true, completion: nil)
+        
+    }
 
 }
 
