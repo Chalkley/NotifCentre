2176b8e5fa9d015bdf051b6e4274d3cb08320e0d
Jason Chalkley <jaychalks@gmail.com>
2019-01-31 15:53:35 +0000

    Notif VC created.

---
+++ NotifCentre/NotifViewController.swift
@@ -0,0 +1,21 @@
+//
+//  NotifViewController.swift
+//  NotifCentre
+//
+//  Created by Jason Chalkley on 31/01/2019.
+//  Copyright © 2019 Jason Chalkley. All rights reserved.
+//
+
+import UIKit
+
+class NotifViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+
+}
--- NotifCentre/ViewController.swift
+++ NotifCentre/ViewController.swift
@@ -12,7 +12,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func setupUI() {
+        let rightNavUtton = UIButton(type: .custom)
+        rightNavUtton.setTitle("Notif", for: .normal)
+        rightNavUtton.setTitleColor(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1), for: .normal)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: rightNavUtton)
+        
     }
 
 
